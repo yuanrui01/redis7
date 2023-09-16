0ca4f06903fe91b9c9b883b5be064541a47cf71e
yuanrui01 <xdyrfree@gmail.com>
2023-09-16 19:54:56 +0800

    代码备份

---
+++ redis7_study/src/main/java/com/foresee/redis7/mapper/CustomerMapper.java
@@ -0,0 +1,8 @@
+package com.foresee.redis7.mapper;
+
+import com.foresee.redis7.entities.Customer;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CustomerMapper extends Mapper<Customer> {
+}
+
